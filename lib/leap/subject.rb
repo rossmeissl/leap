a8ca3ca29657e780a52a09e2789775286b07d424
Seamus Abshere <seamus@abshere.net>
2010-05-12 10:25:26 -0400

    use module_eval instead of define_method because it's a lot easier to understand (and it gets rid of the 0 for 1 block parameters warning)

--- lib/leap/subject.rb
+++ lib/leap/subject.rb
@@ -5,11 +5,14 @@ module Leap
     end
     attr_reader :decisions
     def decide(goal, options = {}, &blk)
-      decisions[goal] = ::Leap::Decision.new goal, options
-      Blockenspiel.invoke(blk, decisions[goal])
-      define_method goal do |considerations|
-        self.class.decisions[goal].make send(self.class.decisions[goal].signature_method), considerations
-      end
+      decisions[goal] = Decision.new goal, options
+      Blockenspiel.invoke blk, decisions[goal]
+      module_eval %{
+        def #{goal}(*args)
+          considerations = args.first
+          self.class.decisions[#{goal.inspect}].make send(self.class.decisions[#{goal.inspect}].signature_method), considerations
+        end
+      }, __FILE__, __LINE__
     end
   end
 end
