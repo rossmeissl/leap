da09d46ca0317b85277093de245d604e7cac9495
Seamus Abshere <seamus@abshere.net>
2010-05-12 10:24:58 -0400

    exploit module

--- lib/leap/subject.rb
+++ lib/leap/subject.rb
@@ -2,10 +2,8 @@ module Leap
   module Subject
     def self.extended(base)
       base.instance_variable_set :@decisions, {}
-      class << base
-        attr_reader :decisions
-      end
     end
+    attr_reader :decisions
     def decide(goal, options = {}, &blk)
       decisions[goal] = ::Leap::Decision.new goal, options
       Blockenspiel.invoke(blk, decisions[goal])
