daa1bd52724c6528b009414dc18f231ed55234a4
Andy Rossmeissl <andy@rossmeissl.net>
2010-05-12 10:50:04 -0400

    Well would you look at that: the splat in a proc signature.

--- lib/leap/subject.rb
+++ lib/leap/subject.rb
@@ -7,7 +7,7 @@ module Leap
     def decide(goal, options = {}, &blk)
       decisions[goal] = ::Leap::Decision.new goal, options
       Blockenspiel.invoke(blk, decisions[goal])
-      define_method goal do |considerations|
+      define_method goal do |*considerations|
         self.class.decisions[goal].make send(self.class.decisions[goal].signature_method), considerations
       end
     end
