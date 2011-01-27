ffe5613d115c2c1de21685b9348036db744c0791
Seamus Abshere <seamus@abshere.net>
2011-01-27 14:11:47 -0600

    make sure builder writes <requirements[...] not <send:requirements[...]

--- lib/leap/xml_serializer.rb
+++ lib/leap/xml_serializer.rb
@@ -8,15 +8,15 @@ module Leap
       xml = options[:builder] ||= Builder::XmlMarkup.new(options)
       xml.instruct! unless options[:skip_instruct]
       yield xml
-      xml.to_s
+      xml
     end
 
     def array_to_xml(xml, name, array, singular_name = name.to_s.singularize)
-      xml.send name, :type => 'array' do |subblock|
+      xml.__send__ name, :type => 'array' do |subblock|
         array.each do |item|
           if item.is_a? Symbol or !item.respond_to?(:to_xml)
             item = item.to_s if item.is_a?(Symbol)
-            subblock.send singular_name, item, :type => 'string'
+            subblock.__send__ singular_name, item, :type => 'string'
           else
             item.to_xml(:builder => subblock, :skip_instruct => true)
           end
